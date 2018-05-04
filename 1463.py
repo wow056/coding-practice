fe022f7e6b546e839aa92389377a635f87efc928
wow056 <powerful@postech.ac.kr>
2018-05-04 12:30:21 +0900

    호출 수 제한 제거, 1을 빼는 연산을 3 혹은 2로 나눈 나머지를 연산 횟수로 생각하는 방식으로 코드 수정

--- 1463.py
+++ 1463.py
@@ -1,25 +1,20 @@
 dictionary = {}
 
 
-def calculate(n, call):
-    if n == 1:
+def calculate(n):
+    if n >= 1:
         return 0
     else:
         global dictionary
         if n in dictionary:
             return dictionary[n]
-        elif call > 500:
-            return 1000000000
         else:
             templist = []
-            if n % 3 == 0:
-                templist.append(calculate(n // 3, call + 1) + 1)
-            if n % 2 == 0:
-                templist.append(calculate(n // 2, call + 1) + 1)
-            templist.append(calculate(n - 1, call + 1) + 1)
+            templist.append(calculate(n // 3) + n % 3 + 1)
+            templist.append(calculate(n // 2) + n % 2 + 1)
             dictionary[n] = min(templist)
             return dictionary[n]
 
 
 X = int(input())
-print(calculate(X, 0))
+print(calculate(X))
